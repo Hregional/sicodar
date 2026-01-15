b653ce9022a61fca0aa30c1525cff1c04e572813
Adolfo Ajucum <martin_aj22@hotmail.com>
2026-01-15 13:38:15 -0600

    feat: configure Vite dev server with allowed hosts and host binding.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['sicodar.hro.gob.gt'],
+    host: true,
+  },
   resolve: {
     alias: {
       '@': '/src',
